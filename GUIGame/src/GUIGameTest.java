76370f7cb9bacd8213eaba743c7996662729ce1a
Eraldo Forgoli <eraldoforgoli@gmail.com>
2019-04-04 22:50:45 +0200

    Update GUIGameTest.java

--- GUIGame/src/GUIGameTest.java
+++ GUIGame/src/GUIGameTest.java
@@ -3,7 +3,7 @@ import javax.swing.JFrame;
 public class GUIGameTest {
 	public static void main(String[] args) {
 
-		JFrame frame = new JFrame();
+		JFrame frame = new JFrame()
 		GUIGame game = new GUIGame();
 		frame.add(game);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
